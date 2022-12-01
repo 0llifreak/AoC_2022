11db78309e7d313cd1e62c7dfa4aeb0fdfa46176
Oliver Werthwein <werthwein@fzi.de>
2022-12-01 08:34:08 +0100

    Rename folder for correct sorting

---
+++ Day 3/AoC_template.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# -- Advent of Code 2022 ---------
+#
+# Puzzle X
+#
+# Author: XXX
+# --------------------------------
+from aocd.models import Puzzle
+
+# --------------------------------
+# Get input data from AoC
+# --------------------------------
+puzzle = Puzzle(year=2022, day=X)
+
+# --------------------------------
+# Solution Part X
+# --------------------------------
