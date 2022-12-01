76e97804f65d13f542a84e4bd2d847ddaa0d2188
Oliver Werthwein <werthwein@fzi.de>
2022-12-01 18:29:36 +0100

    idea: using ctx mgr

--- Day_01/Leo_Day_1.py
+++ Day_01/Leo_Day_1.py
@@ -1,19 +1,24 @@
-import pandas as pd
+# import pandas as pd
 import numpy as np
 
 
 my_file = open("Day_01/input_leo.txt", "r")
-  
 
-data = my_file.read()
+
+# data = my_file.read()
+# data_into_list = data.split("\n")
+# my_file.close()
+
+# Using a context manager
+with open("Day_01/input_leo.txt", "r"):
+    data = my_file.read()
 data_into_list = data.split("\n")
-my_file.close()
 
-i=0
-for element in data_into_list: 
+i = 0
+for element in data_into_list:
     if element == "":
         data_into_list[i] = "0"
-    i=i+1
+    i = i + 1
 
 data_into_list = list(map(int, data_into_list))
 print(data_into_list)
@@ -36,4 +41,3 @@ print("Ergebnis PART 1:", max(totals))
 ####PART 2######################
 
 print("Ergebnis PART 2:", sum(sorted(totals, reverse=True)[:3]))
-
