34add9313d2c47a63e41717f1cda50fbfe44647c
Oliver Werthwein <werthwein@fzi.de>
2022-12-01 18:42:25 +0100

    prep day 2

---
+++ Day_02/olli.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# -- Advent of Code 2022 ---------
+#
+# Puzzle 2
+#
+# Author: Olli
+# --------------------------------
+from aocd.models import Puzzle
+
+# --------------------------------
+# Get input data from AoC
+# --------------------------------
+puzzle = Puzzle(year=2022, day=2)
+
+# --------------------------------
+# Solution Part 1
+# --------------------------------
