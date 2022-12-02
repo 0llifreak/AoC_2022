c1b61c1357711f2dde2e2d6fda47e7912beaffbd
0lli <74310296+0llifreak@users.noreply.github.com>
2022-12-02 09:23:40 +0100

    Solution Part 2

--- Day_02/olli.py
+++ Day_02/olli.py
@@ -70,3 +70,36 @@ print("Score", score)
 # X = Lose
 # Y = Draw
 # Z = Win
+
+score_2 = 0
+for round in rps:
+    opponent = round[0]
+    strategy = round[2]
+
+    # Calculate game score
+    # Lose
+    if strategy == "X":
+        if opponent == "A":
+            score_2 += 3
+        elif opponent == "B":
+            score_2 += 1
+        elif opponent == "C":
+            score_2 += 2
+    # Draw
+    elif strategy == "Y":
+        if opponent == "A":
+            score_2 += 4
+        elif opponent == "B":
+            score_2 += 5
+        elif opponent == "C":
+            score_2 += 6
+    # Win
+    elif strategy == "Z":
+        if opponent == "A":
+            score_2 += 8
+        elif opponent == "B":
+            score_2 += 9
+        elif opponent == "C":
+            score_2 += 7
+
+print("Score 2", score_2)
