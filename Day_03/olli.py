c1d52c33727e58863441c3372a916da2dc960144
0lli <74310296+0llifreak@users.noreply.github.com>
2022-12-03 14:53:21 +0100

    Solution Part 2

--- Day_03/olli.py
+++ Day_03/olli.py
@@ -38,3 +38,21 @@ print("Solution Part 1:", elem_sum)
 # --------------------------------
 # Solution Part 2
 # --------------------------------
+
+elem_sum = 0
+three_elves = []
+for compartment in rucksack:
+    three_elves.append(compartment)
+    if len(three_elves) == 3:
+        first_compartment = three_elves[0]
+        second_compartment = three_elves[1]
+        third_compartment = three_elves[2]
+        item = set(set(first_compartment).intersection(second_compartment).intersection(third_compartment))
+        elem = item.pop()
+        if elem.islower():
+            elem_sum += ord(elem) - 96
+        else:
+            elem_sum += ord(elem) - 38
+        three_elves.clear()
+
+print("Solution Part 2:", elem_sum)
