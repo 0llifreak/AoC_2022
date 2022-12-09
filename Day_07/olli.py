ad443cf8437721f75cab9541cd886ec3fecdcf76
0lli <74310296+0llifreak@users.noreply.github.com>
2022-12-09 14:38:21 +0100

    NOT working at all 😪

--- Day_07/olli.py
+++ Day_07/olli.py
@@ -16,6 +16,12 @@ from anytree import Node, RenderTree, search, Walker, PreOrderIter, Resolver
 puzzle = Puzzle(year=2022, day=7)
 terminal = puzzle.input_data.split("\n")  # convert string to list
 
+with open("Day_07/input.txt") as file:
+    terminal = file.read()
+terminal = terminal.split("\n")  # convert string to list
+terminal.pop(-1)
+
+
 # AoC Example data
 # ------------------------------------------------------------------
 
@@ -45,31 +51,84 @@ terminal = puzzle.input_data.split("\n")  # convert string to list
 # terminal = terminal.split("\n")
 
 # ------------------------------------------------------------------
+terminal = """$ cd /
+$ ls
+dir mtw
+$ cd mtw
+$ ls
+dir dcgpfrsf
+dir gwqm
+188911 hqdssf
+34693 jvvl.rcs
+dir shzgg
+$ cd dcgpfrsf
+$ ls
+47863 dfwthflp.jwq
+203815 dqbqbps.prq
+dir mhqs
+183653 rqjqpm.bbr
+220694 vjw
+$ cd mhqs
+$ ls
+74450 tjpn
+$ cd ..
+$ cd ..
+$ cd gwqm
+$ ls
+dir trghjhvs
+dir shzgg
+$ cd trghjhvs
+$ ls
+dir shzgg
+2620 slwshm.nwr
+$ cd shzgg
+$ ls
+73435 shzgg.bbf
+$ cd ..
+$ cd ..
+$ cd shzgg
+$ ls
+111111 vjwuz
+$ cd ..
+$ cd ..
+$ cd shzgg
+$ ls
+184378 rjjrg.pjq"""
+
+terminal = terminal.split("\n")
 
 # --------------------------------
 # Solution Part 1
 # --------------------------------
 
 root = Node("root", size=0)
-parent_node = root
 dirs = {}
-
+dir_list = []
+parent_node = root
 for cmd in terminal:
+    dirs = {}
     cmd = cmd.split(" ")
     # Generate directory node
     if cmd[0] == "dir":
         dir = Node(cmd[1], parent=parent_node, size=0)
         dirs[cmd[1]] = dir  # save directory name and node for later reference
+        dir_list.append(dirs)
     # Change parent node
     elif cmd[0] == "$":
         if cmd[1] == "cd":
             if cmd[2] == "/":
                 parent_node = root
             elif cmd[2] == "..":
-                parent_node = dir.parent
+                dir_list.pop()
             else:
                 new_dir = f"{cmd[2]}"
-                parent_node = dirs[new_dir]
+                for elem in dir_list:
+                    for name, node in elem.items():
+                        if name == new_dir:
+                            parent_node = node
+                # dir_list.append(dirs)
+                dir_list.append({"dummy": "node"})
+
     # Generate file node
     else:
         file_name = cmd[1]
@@ -77,7 +136,8 @@ for cmd in terminal:
         Node(file_name, parent=parent_node, size=file_size)
 
 # Print graph in terminal
-# print(RenderTree(root).by_attr())
+for pre, _, node in RenderTree(root):
+    print("%s%s" % (pre, node.name))
 
 dir_size = []
 solution2_dir = []
@@ -91,13 +151,13 @@ for dir in search.findall_by_attr(root, 0, "size"):
 
 # print("Solution Size;", dir_size)
 print("Solution 1:", sum(dir_size))
-# 1306611 is right :)
+# # 1306611 is right :)
 
-# --------------------------------
-# Solution Part 2
-# --------------------------------
+# # --------------------------------
+# # Solution Part 2
+# # --------------------------------
 
-# NOT WORKING!!!!
+# # NOT WORKING!!!!
 
 used_space = sum([node.size for node in PreOrderIter(root)])
 free_space = 70000000 - used_space
@@ -114,7 +174,7 @@ for dir in solution2_dir:
 print(good_dirs)
 print("Solution 2:", min(good_dirs))
 
-# 26118278 too high
-# 13371921 too high
+# # 26118278 too high
+# # 13371921 too high
 
-# 10822529 lowest possible value
+# # 10822529 lowest possible value
